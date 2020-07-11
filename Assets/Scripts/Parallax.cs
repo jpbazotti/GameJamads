950ff91dacc644f5c201a8a5557a3a47f92a5fa6
jpbazotti <62719223+jpbazotti@users.noreply.github.com>
2020-07-10 22:09:32 -0300

    manual merge

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+
+    private float length, camHor, camPos;
+    public GameObject cam, spriteOnLeft;
+    public float parallaxEffect;
+
+    void Start()
+    {
+        camPos = cam.transform.position.x;
+        camHor = cam.GetComponent<Camera>().orthographicSize * Screen.width / Screen.height;
+        length = GetComponent<SpriteRenderer>().bounds.size.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = transform.position.x;
+
+        transform.position = new Vector3(x - parallaxEffect, transform.position.y, transform.position.z);
+
+        if (x + length < camPos - camHor)
+        {
+            Debug.Log(spriteOnLeft.transform.position.x + length * 2);
+            transform.position = new Vector3((spriteOnLeft.transform.position.x + length * 2) - 1, transform.position.y, transform.position.z);
+        }
+    }
+}
--- Assets/Scripts/bullet.cs
+++ Assets/Scripts/bullet.cs
@@ -6,6 +6,7 @@ public class bullet : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float vel;
+    public bool enemyShoot;
     void Start()
     {
         rb.velocity = new Vector2(vel, 0);
@@ -14,11 +15,23 @@ public class bullet : MonoBehaviour
   
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("enemy"))
+        if (!enemyShoot)
         {
-            Destroy(gameObject);
+            if (collision.gameObject.CompareTag("enemy"))
+            {
+                Destroy(gameObject);
 
-            Destroy(collision.gameObject);
+                Destroy(collision.gameObject);
+            }
+        }
+        else
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                Destroy(gameObject);
+
+                Destroy(collision.gameObject);
+            }
         }
         if (collision.gameObject.CompareTag("border"))
         {
